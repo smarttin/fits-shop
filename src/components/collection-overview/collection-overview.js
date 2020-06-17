521b0189f72e205d684d7641ba09a9ebec6fd291
smarttin <smarttin77@gmail.com>
2020-06-17 17:41:43 +0100

    adds directory and collection state into redux, adds collectionOverview component

---
+++ src/components/collection-overview/collection-overview.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
+
+import './collection-overview.scss';
+import CollectionPreview from '../collection-preview/collection-preview';
+import { selectCollections } from '../../redux/shop/shop.selector';
+
+const CollectionsOverview = ({ collections }) => (
+  <div className='collections-overview'>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
